f0ab27d278f2b6e2b43d8b1f2951ddc5f2db5b9f
Favio V. F <favio102@protonmail.com>
2024-06-07 11:08:58 -0700

    nav bar updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -9,7 +11,7 @@ const Home = () => {
         PrompterExt is an open-source AI prompting tool for modern world to
         discover, create adn share creative prompts.
       </p>
-      {/* Feed */}
+      <Feed />
     </section>
   );
 };
