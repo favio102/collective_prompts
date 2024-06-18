ea20a0b58aa993e2bdcfda5a833424080a6e57e1
Favio V. F. <favio102@protonmail.com>
2024-06-18 06:42:12 -0700

    crud functionalities added

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -4,8 +4,9 @@ import Prompt from "@models/prompt";
 export const GET = async (request, { params }) => {
   try {
     await connectToDB();
+
     const prompt = await Prompt.findById(params.id).populate("creator");
-    if (!prompt) return new Response("Prompt not found, {status: 404}");
+    if (!prompt) return new Response("Prompt not found", { status: 404 });
 
     return new Response(JSON.stringify(prompt), {
       status: 200,
@@ -16,7 +17,7 @@ export const GET = async (request, { params }) => {
 };
 
 export const PATCH = async (request, { params }) => {
-  const { prompt, tag } = await request.JSON();
+  const { prompt, tag } = await request.json();
 
   try {
     await connectToDB();
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,13 +5,30 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Profile from "@components/profile";
 
-
 const myProfile = () => {
   const router = useRouter();
   const { data: session } = useSession();
   const [allPosts, setAllPosts] = useState([]);
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" });
+        const filteredPosts = allPosts.filter((p) => p._id !== post._id);
+        setAllPosts(filteredPosts);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  };
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
